ddd214d283e15bcf8bc14285477b9bb1a5924c34
xuxiaoyu011 <xuxiaoyu011@163.com>
2024-03-20 22:44:18 +0800

    polars

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 use ndarray::prelude::*;
+use polars::prelude::*;
 
 fn main() {
     let arr1 = array![1., 2., 3., 4., 5., 6.];
@@ -11,4 +12,14 @@ fn main() {
     let arr5 = array![[1.], [2.]];//Array::from_elem((2, 1), 1.);
     let arr6 = arr4 + arr5;
     println!("2D array:\n{}", arr6);
+    
+
+    let df = df![
+        "Model" => ["iPhone XS", "iPhone 12", "iPhone 13", "iPhone 14", "Samsung S11", "Samsung S12", "Mi A1", "Mi A2"],
+        "Company" => ["Apple", "Apple", "Apple", "Apple", "Samsung", "Samsung", "Xiao Mi", "Xiao Mi"],
+        "Sales" => [80, 170, 130, 205, 400, 30, 14, 8],
+        "Comment" => [None, None, Some("Sold Out"), Some("New Arrival"), None, Some("Sold Out"), None, None],
+    ];
+
+    println!("{}", &df.unwrap()); 
 }
